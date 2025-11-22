e3520ae3c6bc1828032a0000bd62a1cf77069343
uziel <20243ds112@utez.edu.mx>
2025-11-22 15:22:55 -0600

    Plugins y implementaciones del proyecto Modificacion del TOML , KTS APP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    /*
+    Se agrega plugin del proyecto
+    Plugin KSP para procesar la base de datos Room
+     */
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -40,10 +45,26 @@ android {
 }
 
 dependencies {
+    //Dependecias que se agregan
 
+    // Necesario para vistas clásicas usadas por Media3
+    implementation(libs.androidx.appcompat)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+    // Media3 (Video y Audio)
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+    // Coil (Imágenes)
+    implementation(libs.coil.compose)
+    // Accompanist (Carrusel)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.pager.indicators)
 
-
-
+    //Aqui son los del proyecto de core ui
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,6 +73,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //Aqui va testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
