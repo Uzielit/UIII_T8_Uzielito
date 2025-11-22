58af767f5e848cff1d37c0c4ada0bda347c05336
uziel <20243ds112@utez.edu.mx>
2025-11-22 17:31:31 -0600

    Cambios Uzielito

---
+++ app/src/main/java/com/uiii_t8/data/DataBase.kt
@@ -0,0 +1,2 @@
+package com.uiii_t8.data
+
---
+++ app/src/main/java/com/uiii_t8/data/MediaItem.kt
@@ -0,0 +1,18 @@
+package com.uiii_t8.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Define el tipo de medio
+enum class MediaType { AUDIO, IMAGE, VIDEO }
+
+@Entity(tableName = "media_items")
+data class MediaItem (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val uri: String, // La URI del archivo (ej. content://...)
+    val name: String,
+    val date: Long, // Fecha de creación (timestamp)
+    val duration: Long, // Duración en ms (0 para imágenes)
+    val type: MediaType // Tipo de medio (AUDIO, IMAGE, VIDEO)
+)
